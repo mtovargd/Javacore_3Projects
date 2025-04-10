8802666d120118f4c55aa983f91b2e8958fecfef
Miguel Angel Tovar <mtovar@nocs-MacBook-Pro.local>
2025-04-10 15:25:14 -0600

    Task 3 complete! Added loops to keep on menu until exit option is selected

--- Simple Search Engine (Java)/task/src/search/Menu.java
+++ Simple Search Engine (Java)/task/src/search/Menu.java
@@ -3,11 +3,14 @@ package search;
 import java.util.Scanner;
 
 public class Menu {
+
         Scanner scanner = new Scanner(System.in);
         DataHandler dataHandler = new DataHandler();
+        boolean running = true;
+
     public void displayMenu() {
         System.out.println("Enter the number of people:");
-        int n = checkNumInput();
+        int n = scanner.nextInt();
         scanner.nextLine();
 
         System.out.println("Enter all the people:");
@@ -15,13 +18,22 @@ public class Menu {
             dataHandler.saveInputs(scanner.nextLine());
         }
 
-        System.out.println("Enter the number of search queries");
-        n = checkNumInput();
-        scanner.nextLine();
-
-        System.out.println("Enter the data to search people");
-        for (int i = 0; i < n; i++) {
-            dataHandler.findPeople(scanner.nextLine());
+        while (running) {
+            switch (checkNumInput()) {
+                case 1:
+                    System.out.println("Enter a name or email to search all suitable people.");
+                    scanner.nextLine();
+                    dataHandler.findPeople(scanner.nextLine());
+                    break;
+                case 2:
+                    System.out.println("=== List of people ===");
+                    scanner.nextLine();
+                    dataHandler.findPeople();
+                    break;
+                case 0:
+                    running = false;
+                    break;
+            }
         }
     }
 
@@ -29,10 +41,15 @@ public class Menu {
         boolean valid = false;
         String input = "";
         while (!valid) {
+            System.out.println("=== Menu ===\n" +
+                    "1. Find a person\n" +
+                    "2. Print all people\n" +
+                    "0. Exit");
             try{
                 input = scanner.next();
                 Integer.parseInt(input);
             } catch (NumberFormatException e) {
+                System.out.println("Incorrect option! Try again.");
                 continue;
             }
             valid = true;
